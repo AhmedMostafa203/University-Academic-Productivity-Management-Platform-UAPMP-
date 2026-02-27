c00b11aebef8887a423a670cbce036899a87f6f9
oda <odaemam4@gmail.com>
2026-02-27 20:26:40 +0200

    edit files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const User = require("../models/User");
 const Universities = require("../constants/universities");
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 // ============================================
@@ -262,7 +262,7 @@ router.post("/register", async (req, res) => {
 
     // Send verification email (link contains user id; verification checks createdAt)
     try {
-      await sendVerificationEmail(lowerEmail, newUser._id, displayName);
+      await sendVerificationEmail(emailLower, newUser._id, displayName);
     } catch (emailError) {
       // Log email error but don't fail registration
       console.error(
@@ -490,5 +490,73 @@ router.get("/verify-email/:uid", async (req, res) => {
       );
   }
 });
+
+
+
+
+
+//login phase
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;       
+if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+        
+      });
+    }
+    const lowerEmail = email.toLowerCase();
+    const user = await User.findOne({ email:lowerEmail });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+    if (!user.isEmailVerified) {
+      return res.status(403).json({
+        message: "Email not verified",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+     const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+   
+    res.status(500).json({
+      message: "An unexpected error occurred during login",
+    });
+  }  
+});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
